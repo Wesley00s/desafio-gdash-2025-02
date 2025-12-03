c19000d0e189bf6847011249fbd359b8a82cf91d
Wesley00s <wesley300rodrigues@gmail.com>
2025-12-03 20:00:01 -0300

    feat(backend/src): Initialize NestJS application

---
+++ backend/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { WeatherModule } from './weather/weather.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+   imports: [
+      ConfigModule.forRoot(),
+      MongooseModule.forRoot(
+         process.env.MONGO_URI || 'mongodb://localhost:27017/gdash',
+      ),
+      WeatherModule,
+      UsersModule,
+      AuthModule,
+   ],
+   controllers: [],
+   providers: [],
+})
+export class AppModule {}
